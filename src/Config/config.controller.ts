0861e0030dd16498825c04029d65d63d715cc34f
Nguyễn Ngọc Chinh <47266403+NguyenNgocChinh@users.noreply.github.com>
2023-11-20 09:26:52 +0700

    Update config.controller.ts

--- src/Config/config.controller.ts
+++ src/Config/config.controller.ts
@@ -7,14 +7,14 @@ export class ConfigController {
   @Get()
   @Render('config')
   config() {
-    const filePath = path.join(process.cwd(), '/config.json');
+    const filePath = path.join('/tmp', '/config.json');
     const configFile = fs.readFileSync(filePath, 'utf-8').toString();
     return { config: JSON.parse(configFile) };
   }
 
   @Post()
   saveConfig(@Body() body) {
-    // const filePath = path.join(process.cwd(), '/config.json');
+    // const filePath = path.join('/tmp', '/config.json');
     // const configFile = fs.writeFileSync(filePath, body);
     const filePath = path.join(process.cwd(), '/config.json');
     fs.writeFileSync(filePath, JSON.stringify(body));
