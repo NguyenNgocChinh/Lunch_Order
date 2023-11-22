3299654f407e4b74caba4e9883cb828489f7c8fd
Chinh Nguyen Ngoc <hopee.chinhnguyen@gmail.com>
2023-11-23 02:32:23 +0700

    Relative path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { IndexModule } from './index/index.module';
 import { ConfigModule } from '@nestjs/config';
-import { RestaurantsModule } from 'src/restaurants/restaurants.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
